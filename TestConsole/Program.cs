b30b723c6247f56c6050adda3eda20cc3c4bc3ef
MATH RUDN <rudn.math@outlook.com>
2020-02-13 15:28:15 +0300

    init

---
+++ TestConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+            Console.WriteLine("Введите >");
+            String str = Console.ReadLine();
+            Console.WriteLine("Введено:" + str);
+            Console.ReadLine();
+        }
+    }
+}
